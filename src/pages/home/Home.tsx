817901ad262617c18796d34ba15272821377460f
Brennan Pilcher <brennan.pilcher@gmail.com>
2023-01-09 21:36:01 -0500

    Homepage copy and styling adjustments

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -6,7 +6,10 @@ const Home = () => {
     <>
       <Header />
       <div className='hero'>
-        <span>I'm a <span className='highlight'>technologist</span> with a background in <span className='highlight'>art and design</span> who enjoys exploring the interactions between creative media and technology.</span>
+        <span>I'm a <span className='highlight'>full stack developer</span> with a background in <span className='highlight'>art and design</span>, currently working as a Senior Consultant at <a href='https://www.thoughtworks.com' target='_blank' rel='noreferrer' className='link'>Thoughtworks</a>.</span>
+      </div>
+      <div className='hero'>
+        <span>I'm passionate about the power of technology to enable creativity.</span>
       </div>
     </>
   );
